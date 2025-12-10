26e6175931f6c51d6a2dfa56d703d37175dfda68
lawr0ne <lawr0ne.local@gmail.com>
2025-12-11 00:00:50 +0100

    chore: added tailwindcss, zustand, and some path aliases

---
+++ frontend/src/components/buttons/Button.tsx
@@ -0,0 +1,11 @@
+import { useThemeStore } from "@lib/stores";
+
+const theme = useThemeStore(state => state.theme);
+
+function Button() {
+    return (
+        {}
+    )
+}
+
+export default Button;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                theme: {
+                    white: {
+                        // TODO: Color pallete
+                    },
+                    dark: {
+                        // TODO: Color pallete
+                    }
+                }
+            }
+        }
+    }
+}
