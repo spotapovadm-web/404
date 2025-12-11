88199476125a77f6e8e55b9d591c06d5899a0b8c
lawr0ne <lawr0ne.local@gmail.com>
2025-12-11 23:25:58 +0100

    last changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,8 +8,8 @@ function Home() {
         <div className="w-screen h-screen flex flex-col gap-2 items-center justify-start">
             <h1 className="font-bold text-4xl pt-[10vh]">TestOps Copilot</h1>
             <h2 className="text-gray-400 text-3xl">by 404 Team</h2>
-            <Button onClick={() => navigate('/prompt')} className="mt-[30vh]">
-                <a className="text-gray-600 font-bold text-2xl">Открыть</a>
+            <Button onClick={() => navigate('/prompt')} className="mt-[30vh] lg:w-75 lg:h-25">
+                <a className="text-gray-600 font-bold text-2xl lg:text-5xl">Открыть</a>
             </Button>
         </div>
     );
