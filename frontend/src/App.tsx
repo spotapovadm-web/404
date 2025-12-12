f17f0d25a51e81be1a0257571c5ef4ab80ebbbef
lawr0ne <lawr0ne.local@gmail.com>
2025-12-12 06:32:42 +0100

    configure toasts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
     <div className="h-screen w-screen">
       <Outlet />
+      <Toaster position="bottom-center" toastOptions={{
+        style: {
+          background: '#316685',
+          color: '#ffffff'
+        }
+      }} />
     </div>
   );
 }
